ae42f52c8f1e92d924adecbe218e775e7edf9bb8
Travis Nagle <itsnagle@gmail.com>
2023-03-25 15:16:51 -0400

    Program XML added

--- WarehouseSimulation/Program.cs
+++ WarehouseSimulation/Program.cs
@@ -1,7 +1,19 @@
-﻿namespace WarehouseSimulation
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Travis Nagle, Naglet@etsu.edu
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Project 4 - Warehouse Simulation
+// Description: Driver that runs the sim in the console
+//
+///////////////////////////////////////////////////////////////////////////////
+namespace WarehouseSimulation
 {
     internal class Program
     {
+        /// <summary>
+        /// Main method that creates a new warehouse and runs the sim
+        /// </summary>
+        /// <param name="args">Command line arguments</param>
         static void Main(string[] args)
         {
             Warehouse warehouse = new Warehouse();
