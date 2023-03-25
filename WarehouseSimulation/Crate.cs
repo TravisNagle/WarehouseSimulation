4bea884a7785646c9e96d73f1afbb39529771ee6
Travis Nagle <itsnagle@gmail.com>
2023-03-25 15:12:43 -0400

    Crate XML added

--- WarehouseSimulation/Crate.cs
+++ WarehouseSimulation/Crate.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Travis Nagle, Naglet@etsu.edu
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Project 4 - Warehouse Simulation
+// Description: Crate class that keeps track of the crate ID and value
+//
+///////////////////////////////////////////////////////////////////////////////
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +14,15 @@ using System.Threading.Tasks;
 
 namespace WarehouseSimulation
 {
+    /// <summary>
+    /// Implementation of a crate object 
+    /// </summary>
     internal class Crate
     {
         public string _id = "";
+        /// <summary>
+        /// ID property that randomly sets the crates ID to 4 numbers between 0-9
+        /// </summary>
         public string Id 
         { 
             get
@@ -26,6 +40,9 @@ namespace WarehouseSimulation
             }
         }
         public double _price;
+        /// <summary>
+        /// Price property that sets a random crate value between 50-500
+        /// </summary>
         public double Price
         {
             get
